ad8d0d5a9560bcc252917e2ddb6f15b43c88606e
raissarossi <109825210+raissarossi@users.noreply.github.com>
2022-09-14 09:16:40 -0300

    Update Main.java

--- aula01/src/Bosch/Main.java
+++ aula01/src/Bosch/Main.java
@@ -6,16 +6,16 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
 
     public static void main(String[] args) {
-        /*
+        
         int raio=5;
         final double pi =3.141;
         double areaCirculo=pi*raio*raio;
         System.out.printf("A área do circulo é %f e o raio é %d\n", areaCirculo,raio);
         System.out.println("A área do circulo é"+ areaCirculo+ " e a medida do raio é"+raio);
-        */
+        
 
         //TIPOS DE VARIAVEIS INTEIROS
-        /*
+        
         byte meuByte=127;
         System.out.printf("Tamanho do Byte: %d\n", meuByte);
         short meuShort=32767;
@@ -26,30 +26,30 @@ public class Main {
         System.out.printf("Tamanho do Long: %d\n", meuLong);*/
 
         //TIPOS DE VARIAVEIS REAIS
-        /*
+        
         float meuFloat=3.4e-38f;
         System.out.printf("Valor do float %f\n", meuFloat);
         double meuDouble=1.7e+308;
         System.out.printf("Valor de Double %f\n", meuDouble);
         System.out.printf(Math.PI);
-        */
+        
 
         //TIPOS DE CARACTERES
-        /*
+        
         char meuChar='A';
         System.out.printf("Meu char é %c\n",meuChar);
         boolean meuBoolean=true;
         System.out.printf("Meu booleano é %b\n",meuBoolean);
-        */
+        
 
         //TIPOS DE STRING
-        /*
+        
         String minhaString="Bosch";
         System.out.printf("Minha String é %s\n",minhaString);
-        */
+        
 
         //TIPOS DE VARIÁVEIS
-        /*
+        
         var a=10;
         System.out.println(a);
         var b=3.14;
@@ -62,7 +62,7 @@ public class Main {
         System.out.println(d);
         var e="Bosch";
         System.out.println(e);
-*/
+
         double num1=10;
         double num2=3;
         System.out.println(num1+num2);
@@ -75,6 +75,16 @@ public class Main {
         double potencia;
         potencia= Math.pow(10,3);
         System.out.println(potencia);
+        
+        //EXEMPLOS DE USO DOS SINAIS MAT
+        
+        System.out.println(10+2*20);
+        System.out.println(10+2==6*2);
+        int x=3, y=4, z=5;
+        System.out.println(x<y && z>y);
+        System.out.println(!(x<y) == x>y);
+        System.out.println(!(y<x) && x>z || z<y);
+        System.out.println(x == z - y && z != y - x || y != z - x);
 
     }
 }
